a5c0baa7f8efb0d50a84cc1525b20e44437c71cf
Vladimir Dejanovic <ed.wong.iv@gmail.com>
2018-12-01 20:44:31 +0100

    added init graphql

---
+++ src/main/java/xyz/itshark/conf/talk/graphqltorescue/graphql/Query.java
@@ -0,0 +1,40 @@
+package xyz.itshark.conf.talk.graphqltorescue.graphql;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import xyz.itshark.conf.talk.graphqltorescue.pojo.Attendee;
+import xyz.itshark.conf.talk.graphqltorescue.pojo.Speaker;
+import xyz.itshark.conf.talk.graphqltorescue.pojo.Talk;
+import xyz.itshark.conf.talk.graphqltorescue.service.AttendeeService;
+import xyz.itshark.conf.talk.graphqltorescue.service.SpeakerService;
+import xyz.itshark.conf.talk.graphqltorescue.service.TalkService;
+
+import java.util.List;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+
+    @Autowired
+    TalkService talkService;
+
+    @Autowired
+    SpeakerService speakerService;
+
+    @Autowired
+    AttendeeService attendeeService;
+
+    public List<Talk> allTalks() {
+        return talkService.findAll();
+    }
+
+
+    public List<Speaker> allSpeakers() {
+        return speakerService.findAll();
+    }
+
+    public List<Attendee> allAttendees() {
+        return attendeeService.findAll();
+    }
+
+}
